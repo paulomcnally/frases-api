e0158e1401478ee20cab8a2486d736d31e46fefd
Paulo McNally <paulomcnally@gmail.com>
2015-12-12 00:55:06 -0600

    return url

--- common/models/background.js
+++ common/models/background.js
@@ -2,7 +2,9 @@ module.exports = function(Background) {
 
   Background.random = function(cb) {
     var query = 'SELECT url FROM background ORDER BY RANDOM() LIMIT 1';
-    Background.dataSource.connector.query(query, cb);
+    Background.dataSource.connector.query(query, function(err, rows) {
+      cb(null, rows[0].url);
+    });
   };
 
   var randomOptions = {
