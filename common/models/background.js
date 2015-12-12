5166fb6d2978fea970de040b437de0feb814d03d
Paulo McNally <paulomcnally@gmail.com>
2015-12-12 00:47:57 -0600

    git push heroku master

--- common/models/background.js
+++ common/models/background.js
@@ -1,3 +1,16 @@
 module.exports = function(Background) {
 
+  Background.random = function(cb) {
+    var query = 'SELECT id FROM background ORDER BY RAND() LIMIT 1';
+    Background.dataSource.connector.query(query, cb);
+  };
+
+  var randomOptions = {
+    returns: {
+      arg: 'greeting',
+      type: 'string'
+    }
+  };
+  
+  Background.remoteMethod('random', randomOptions);
 };
