7a5339c2b4fe56c8199597b38e2c09891d82ca7e
Paulo McNally <paulomcnally@gmail.com>
2015-12-13 14:43:53 -0600

    added type to background model

--- common/models/background.js
+++ common/models/background.js
@@ -1,13 +1,17 @@
 module.exports = function(Background) {
 
-  Background.random = function(cb) {
-    var query = 'SELECT url FROM background ORDER BY RANDOM() LIMIT 1';
+  Background.random = function(type, cb) {
+    var query = "SELECT url FROM background WHERE type = '" + type + "' ORDER BY RANDOM() LIMIT 1";
     Background.dataSource.connector.query(query, function(err, rows) {
       cb(null, rows[0]);
     });
   };
 
   var randomOptions = {
+    accepts: {
+      arg: 'type',
+      type: 'string'
+    },
     returns: {
       type: 'object',
       root: true
