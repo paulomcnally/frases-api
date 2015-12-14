64f8b29889915146aebffc41a5318955c37248b5
Paulo McNally <paulomcnally@gmail.com>
2015-12-13 20:29:14 -0600

    added custom query

--- common/models/post.js
+++ common/models/post.js
@@ -1,3 +1,29 @@
 module.exports = function(Post) {
 
+  Post.feed = function(skip, cb) {
+    skip = skip || 0;
+
+    var query = "SELECT url, type FROM post WHERE type = '" + type + "' ORDER BY RANDOM() LIMIT 10 OFFSET " + skip;
+    Post.dataSource.connector.query(query, function(err, rows) {
+      cb(null, rows);
+    });
+  };
+
+  var feedOptions = {
+    accepts: {
+      arg: 'type',
+      type: 'string'
+    },
+    http: {
+        verb: 'get',
+        source: 'query'
+    },
+    returns: {
+      type: 'object',
+      root: true
+    }
+  };
+
+  Post.remoteMethod('feed', feedOptions);
+
 };
